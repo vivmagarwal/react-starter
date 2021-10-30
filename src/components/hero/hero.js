dda9e225879e09f45d475fad5e0a7234a87fb7c5
drupalastic <vivsurat@gmail.com>
2021-10-30 21:40:08 +0530

    phase1: static components added.

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,26 +1,26 @@
 function Hero() {
   return (
     <div class="hero">
-      <div class="hero__container container">
-        <div class="hero__text">
-          <h2 class="hero__title">
-            Endless entertainment and knowledge
-          </h2>
+    <div class="hero__container container">
+      <div class="hero__text">
+        <h2 class="hero__title">
+          Endless entertainment and knowledge
+        </h2>
 
-          <div class="hero__subtitle">
-            Read or listen anytime, anywhere.
-          </div>
+        <div class="hero__subtitle">
+          Read or listen anytime, anywhere.
+        </div>
 
-          <a href="#" class="hero__btn btn btn--large btn--white">
-            Read free for 30 days
-          </a>
+        <a href="#" class="hero__btn btn btn--large btn--white">
+          Read free for 30 days
+        </a>
 
-          <div class="hero__under-btn under-btn">
-            Only ₹299/month after. Cancel anytime.
-          </div>
+        <div class="hero__under-btn under-btn">
+          Only ₹299/month after. Cancel anytime.
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
