53de2b8055a9fa2ffe08c54b07ca04630d70b8f4
drupalastic <vivsurat@gmail.com>
2021-10-30 23:43:10 +0530

    reusable Button component added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,20 @@
 import { useContext } from "react";
 import SidebarVisibilityContext from "../../store/sidebarVisibilityContext";
+import Button from "../button/button";
 
 function Header() {
-  const [,, toggleSidebarVisibility] = useContext(SidebarVisibilityContext);
+  const [, , toggleSidebarVisibility] = useContext(SidebarVisibilityContext);
   return (
     <>
       <header className="site-header" id="site-header">
         <div className="container">
-          <button onClick={toggleSidebarVisibility} className="btn--clear hamburger hamburger--light site-header__toggle-sidebar">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
           
+          <Button onClick={toggleSidebarVisibility} className="btn--clear hamburger hamburger--light site-header__toggle-sidebar">
+            <span></span>
+            <span></span>
+            <span></span>
+          </Button>
+
           <a href="https://google.com" className="site-header__logo">
             <img className="site-header__logo-image" alt="Open library logo" src="https://i.ibb.co/7g0zZ94/open-library-logo.png" />
           </a>
--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,26 +1,28 @@
+import Button from "../button/button";
+
 function Hero() {
   return (
     <div className="hero">
-    <div className="hero__container container">
-      <div className="hero__text">
-        <h2 className="hero__title">
-          Endless entertainment and knowledge
-        </h2>
+      <div className="hero__container container">
+        <div className="hero__text">
+          <h2 className="hero__title">
+            Endless entertainment and knowledge
+          </h2>
 
-        <div className="hero__subtitle">
-          Read or listen anytime, anywhere.
-        </div>
+          <div className="hero__subtitle">
+            Read or listen anytime, anywhere.
+          </div>
 
-        <button className="hero__btn btn btn--large btn--white">
-          Read free for 30 days
-        </button>
+          <Button className="hero__btn btn" size="large" color="white">
+            Read free for 30 days
+          </Button>
 
-        <div className="hero__under-btn under-btn">
-          Only ₹299/month after. Cancel anytime.
+          <div className="hero__under-btn under-btn">
+            Only ₹299/month after. Cancel anytime.
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
