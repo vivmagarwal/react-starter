0c8705ada89c9794cd14b50d40884085fb86cdae
drupalastic <vivsurat@gmail.com>
2021-10-31 10:37:09 +0530

    hero data made dynamic and carlist cleaned up.

--- src/components/backdrop/backdrop.js
+++ src/components/backdrop/backdrop.js
@@ -5,7 +5,7 @@ import SidebarVisibilityContext from "../../store/sidebarVisibilityContext";
 function Backdrop() {
   const [sidebarVisibility,, toggleSidebarVisibility] = useContext(SidebarVisibilityContext);
 
-  return (
+  return sidebarVisibility && (
     <div
       className={`overlay sidebar-overlay ${sidebarVisibility ? 'overlay--show' : ''}`}
       id="sidebar-overlay"
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,19 +1,22 @@
-function Card() {
+function Card({ title, image, description, authorName, authorURL }) {
   return (
     <div className="card">
-      <a href="https://google.com" className="card__title">The Afganisthan Papers: A secret history of the war</a>
+      <a href="https://google.com" className="card__title">{title}</a>
+
       <img
         className="card__img"
-        alt="my good book"
-        src="https://imgv2-1-f.scribdassets.com/img/audiobook_square_badge/510417754/396x396/e1986f8af6/1632395555?v=1"
+        alt="awesome book cover"
+        src={image}
       />
+
       <div className="card__description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero deleniti incidunt rem. Voluptate, tempore
-        quos, suscipit temporibus autem sapiente.
+        {description}
       </div>
-      <a className="card__author" href="https://google.com">Author Name</a>
+
+      <a className="card__author" href={authorURL}>{authorName}</a>
     </div>
   )
 }
+
 
 export default Card;
