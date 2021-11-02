fe456c0ab8a50ab7414bab6c8dcda8c45758fd78
drupalastic <vivsurat@gmail.com>
2021-11-02 19:35:40 +0530

    routing and first post query added.

---
+++ src/pages/list.js
@@ -0,0 +1,13 @@
+import CardList from "../components/cardList/cardList";
+import SectionHeading from "../components/sectionHeading/sectionHeading";
+
+function List() {
+  return (
+    <div className="page__list">
+      <SectionHeading />
+      <CardList />
+    </div>
+  )
+}
+
+export default List;
