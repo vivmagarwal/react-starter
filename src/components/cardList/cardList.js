3da0ab4f1cb563f1334a369b97f2825a8753a9e4
drupalastic <vivsurat@gmail.com>
2021-11-02 15:56:54 +0530

    card component.

--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -18,9 +18,9 @@ function CardList() {
   return (
     <div className="container card-list-wrapper">
       <div className="card-list">
-        {bookList.map((book) => (
+        {bookList.length > 0 ? bookList.map((book) => (
           <Card key={book.id} title={book.book_title} image={  book.cover_url } description={ book.book_short_description } authorName={ book.authors[0].author_name } authorURL= 'https://google.com' />
-        ))}
+        )): <h2>Data loading...........</h2>}
       </div>
     </div>
   )
