16e07cb8e2a662512b2a856ff805cb471b8c9e16
drupalastic <drupalastic@904822.no-reply.drupal.org>
2021-11-13 11:52:54 +0530

    react router - url parameters

--- src/components/sectionHeading/sectionHeading.js
+++ src/components/sectionHeading/sectionHeading.js
@@ -1,8 +1,8 @@
-function SectionHeading() {
+function SectionHeading({book_title}) {
   return (
     <div className="section-heading-block">
       <div className="section-heading-block__container container">
-        <h3 className="section-heading">Category Name here</h3>
+        <h3 className="section-heading"> {book_title ? book_title : 'Section Name here'}</h3>
       </div>
     </div>
   )
